59549f44407e3b0274287326d6df1be958223552
Robert <rquinones93@yahoo.com>
2018-03-09 17:18:54 -0800

    - Rough Draft Sign Up Page - Changed logo on Login Page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'UNO' });
 });
 
+router.get('/signup', function( req, res, next ) {
+  res.render('signup', { title: 'UNO - Sign Up' });
+});
 module.exports = router;
