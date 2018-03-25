94970113868bae2e3e3083895a7e844670b27606
jondo <jdombrowski416@gmail.com>
2018-03-25 15:20:01 -0700

    adding tentative elements. looking into chat and table generation

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/signup', function( req, res, next ) {
   res.render('signup', { title: 'UNO - Sign Up' });
 });
 
+router.get('/main_lobby', function( req, res, next ) {
+  res.render('main_lobby', { title: 'UNO - Lobby' });
+});
+
 module.exports = router;
