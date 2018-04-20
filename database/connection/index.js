91487709c486a134eb60313f47f85c11b4c840dd
Robert <rquinones93@yahoo.com>
2018-04-19 21:40:09 -0700

    Added new directories for users and connection in db

---
+++ database/connection/index.js
@@ -0,0 +1,3 @@
+const pgp = require( 'pg-promise' )();
+const connection = pgp( process.env.DATABASE_URL );
+module.exports = connection;
--- migrations/20180408011617-create-user.js
+++ migrations/20180408011617-create-user.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       profile_picture: {
-        type: Sequelize.BLOB
+        type: Sequelize.STRING
       },
       total_score: {
         type: Sequelize.INTEGER
