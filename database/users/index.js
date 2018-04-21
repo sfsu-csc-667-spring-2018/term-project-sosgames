7c6ce5bc5a36c954ebe983ac272cf8d5ffc2ff37
Robert <rquinones93@yahoo.com>
2018-04-21 10:56:55 -0700

    Added hopeful logic for email and username validation

--- database/users/index.js
+++ database/users/index.js
@@ -2,11 +2,15 @@ const db = require( '../connection' );
 
 // Methods to Define
 const ALL = `SELECT * FROM users`;
+const GETEMAIL = `SELECT email FROM users WHERE email = $1`;
+const GETUSERNAME = `SELECT username FROM users WHERE username = $1`;
 const CREATEUSER = `INSERT INTO users ( username, password, profile_picture_path, total_score, email ) ` +
                    `VALUES ( $1, $2, $3, $4, $5 ) RETURNING *`;
 
 module.exports = {
   all: () => db.any( ALL ),
+  getOneByEmail:  ( email ) => db.oneOrNone( GETEMAIL, [ email ] ),
+  getOneByUsername: ( username ) => db.oneOrNone( GETUSERNAME, [ username ] ),
   // Unsure about how long this is
   createUser: ( username, password, profile_picture_path, total_score, email ) => 
               db.one( CREATEUSER, [ username, password, profile_picture_path, total_score, email ] )
--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const promise = require('bluebird');
+
 
 // User Model
 const {
@@ -14,58 +16,89 @@ router.get('/', function (request, response, next) {
   });
 });
 
-router.post('/', function (request, response) {
+router.post('/', function (request, response, next) {
+  let errors = [];
+  errors = formValidation(request);
 
-  // This is for debugging
-  console.log(request.body);
+  if (!errors) {  
+    errors = [];  
+    
+    if (uniqueEmailAddress(request.body.email)) {
+      errors.push({
+        param: request.body.email,
+        msg: "Email address is already in use.",
+        value: request.body.email
+      });
+    }
 
-  const username = request.body.username;
-  const email = request.body.email;
-  const password = request.body.password;
-  const confirmpassword = request.body.confirmpassword;
-
-  // Validation
-  let errors = validateUser( request, email, password, confirmpassword );
+    if (uniqueUsername(request.body.username)) {
+      errors.push({
+        param: request.body.username,
+        msg: "Username is already in use.",
+        value: request.body.username
+      });
+    }
+  }
 
   if (errors) {
-    console.log(errors);
-
     response.render('signup', {
       title: 'UNO - Sign Up',
       errors: errors
     });
   } else {
-    // Get the Path
-    // Create a User object
-    User.createUser(username, password, '/', 0, email)
+    User.createUser(request.body.username, request.body.password, '/', 0, request.body.email)
       .then(users => {
-        console.log('User id: ' + users.id + ' created in DB.');
-        // This needs to change to be consistent with how I'm displaying errors
         request.flash('success_msg', "You are registered and can now login");
         response.redirect('/');
-      })
-      .catch(error => {
-        console.log(error);
-        response.render('signup', {
-          title: 'UNO - Sign Up',
-          errors: errors
-        });
       });
-
   }
 });
 
 // Validate User
-let validateUser = (( request, email, password, confirmpassword ) => {
-  request.checkBody('email', 'Email is not valid').isEmail();
-  request.checkBody('confirmpassword', 'Passwords do not match').equals(password);
-  let errors = request.validationErrors();
-
-  return errors;
+let formValidation = ((request) => {
+  request.checkBody('username', 'Username field cannot be empty.').notEmpty();
+  request.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+  request.checkBody('email', 'Email is not valid.').isEmail();
+  request.checkBody('email', 'Email address must be between 4-100 characters long.').len(4, 100);
+  request.checkBody('password', 'Password must be 8-100 characters long.').len(8, 100);
+  request.checkBody('confirmpassword', 'Password must be 8-100 characters long.').len(8, 100);
+  request.checkBody('confirmpassword', 'Passwords do not match').equals(request.body.password);
+  return request.validationErrors();
 });
 
-let emailExists = (( email ) => {
-  
+
+let uniqueEmailAddress = (email => {
+  let test = User.getOneByEmail(email)
+    .then(data => {
+      if (data == null) {
+        console.log('UNIQUE USERNAME');
+        return true;
+      } else {
+        console.log('NONUNIQUE EMAIL');
+        return false;
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+});
+
+let uniqueUsername = (username => {
+  return User.getOneByUsername(username)
+    .then(data => {
+      if (data == null) {
+        console.log('UNIQUE USERNAME');
+        console.log(data);
+
+        return true;
+      } else {
+        console.log('NONUNIQUE USERNAME');
+        console.log(data);
+
+        return false;
+      }
+    }).catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
