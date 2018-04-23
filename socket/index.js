8009a7f758255ff9f200a5631614043f773c24f9
jondo <jdombrowski416@gmail.com>
2018-04-23 13:45:56 -0700

    messages now log and display to the console. Next step is styling and setting up the infrastructure for other elements to be passed in and out

--- socket/index.js
+++ socket/index.js
@@ -16,6 +16,7 @@ const init = ( app, server ) => {
     io.on('connection', function(socket){
       socket.on('chat message', function(msg){
         console.log('message: ' + msg);
+        io.emit('chat message', msg);
       });
     });
     
