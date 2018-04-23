633027e710bd8f8da6f6188181b54229e240b72c
jondo <jdombrowski416@gmail.com>
2018-04-23 16:50:15 -0700

    chat output now into a scrollable table

--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,5 @@
 const socketIo = require( 'socket.io' )
-const { USER_JOINED, MESSAGE_SEND } = require( '../constants/events' )
+// const { USER_JOINED, MESSAGE_SEND } = require( '../constants/events' )
 
 const init = ( app, server ) => {
   const io = socketIo( server )
@@ -13,17 +13,16 @@ const init = ( app, server ) => {
       console.log( 'client disconnected' )
     })
 
-    io.on('connection', function(socket){
-      socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-      });
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
     });
+
     
-    // //idea for how to manage gamestate updates
-    // socket.on('game_state_update', ({GAME_ID, GAME_STATE})=>{
-    //   socket.emit('game_state_update',{GAME_STATE})
-    // })
+    //idea for how to manage gamestate updates
+    socket.on('game_state_update', ({GAME_ID, GAME_STATE})=>{
+      socket.emit('game_state_update',{GAME_ID, GAME_STATE})
+    })
   })
 }
 
