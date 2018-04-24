1c3de6a8ab5b5a279973064c316f7abfc8acaa0a
Robert <rquinones93@yahoo.com>
2018-04-23 23:46:48 -0700

    Refactored post request code. Commented section works, working on better logic consolidation.

--- database/users/index.js
+++ database/users/index.js
@@ -1,4 +1,5 @@
 const db = require( '../connection' );
+const bcrypt = require('bcrypt');
 
 // Methods to Define
 const ALL = `SELECT * FROM users`;
@@ -9,18 +10,45 @@ const GETUSERDATA = `SELECT * FROM users WHERE username = $1`;
 const CREATEUSER = `INSERT INTO users ( username, password, profile_picture_path, total_score, email ) ` +
                    `VALUES ( $1, $2, $3, $4, $5 ) RETURNING *`;
 
+// This code works with the commented out section in signup.js
+// module.exports = {
+//   all: () => db.any( ALL ),
+//   isEmailInUse:  ( email ) => db.one( GETEMAIL, [ email ] ),
+//   isUserNameInUse: ( username ) => db.one( GETUSERNAME, [ username ] ),
+//   getUserData: ( username ) => db.one( GETUSERDATA, [ username ] ),
+//   getUserId: ( username ) => db.one( GETUSERID, [ username ] ),
+//   // Unsure about how long this is
+//   createUser: ( username, password, profile_picture_path, total_score, email ) => 
+//               db.one( CREATEUSER, [ username, password, profile_picture_path, total_score, email ] )
+// };
+
 module.exports = {
-  all: () => db.any( ALL ),
   isEmailInUse:  ( email ) => db.one( GETEMAIL, [ email ] ),
   isUserNameInUse: ( username ) => db.one( GETUSERNAME, [ username ] ),
   getUserData: ( username ) => db.one( GETUSERDATA, [ username ] ),
   getUserId: ( username ) => db.one( GETUSERID, [ username ] ),
-  // Unsure about how long this is
+  // Error created - when a post request is made, "ReferenceError: isUserNameInUse is not defined
+  //  at Object.create" is shown
+  create: ( username, email, password, photo_path ) =>
+          Promise.all( [ isEmailInUse( email ), isUserNameInUse( username )] 
+                 .then( ([ emailUsed, usernameUsed ]) => {
+                    console.log('a');
+                    let errors = [];
+                    if (emailInUse || userNameInUse) {
+                      if (emailInUse) {
+                        errors.push({msg: "Email address is already in use."});
+                      }
+                      if (userNameInUse) {
+                        errors.push({msg: "Username is already in use."});
+                      }
+                      Promise.reject( errors );
+                    } else {
+                      bcrypt.hash(password, 10).then(hash => {
+                        Promise.resolve(User.createUser( username, hash, photo_path, 0, email));
+                      });
+                    }
+                 })),
+
   createUser: ( username, password, profile_picture_path, total_score, email ) => 
-              db.one( CREATEUSER, [ username, password, profile_picture_path, total_score, email ] )
+              db.one( CREATEUSER, [ username, password, profile_picture_path, 0, email ] )
 };
-
-
-// module.exports = {
-//   isEmailInUse: email => db.one("SELECT COUNT(*) FROM users WHERE email=$1", email).then( result => result === 1).catch( error => true )
-// }
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
-const { User } = require('../database');
+const {
+  User
+} = require('../database');
 
 router.get('/', function (request, response, next) {
   response.render('signup', {
@@ -11,66 +12,56 @@ router.get('/', function (request, response, next) {
   });
 });
 
-router.post('/', function (request, response, next) {
+router.post('/', (request, response, next) => {
   let formErrors = formValidation(request);
 
   if (formErrors) {
     renderErrors(response, formErrors);
   } else {
-    let errors = []; 
-    // Just trying to get the email value from the DB and save it
-    User.isEmailInUse(request.body.email)
-      .then((data) => {
-        // Email is NOT unique, check username next
-        errors.push({
-          msg: "Email address is already in use."
+    const { username, email, password } = request.body;
+    const photo_path = '/'; // DEBUG This will be updated
+
+    // This is test code, the bottom commented out code works as intended
+    try {
+      User.create( username, email, password, photo_path )
+        .then( errors => {
+          renderErrors(response, errors);
+
+        }).catch( error => {
+          request.flash('success_msg', "You are registered and can now login");
+          response.redirect('/');
+
         });
+    } catch ( e ) { console.log(e);}
+    next();
+    // Promise.all([
+    //   User.isEmailInUse(email),
+    //   User.isUserNameInUse(username)
+      
+    // ]).then(([emailInUse, userNameInUse]) => {
+    //   let errors = [];
+    //   if (emailInUse || userNameInUse) {
+    //     if (emailInUse) {
+    //       errors.push({msg: "Email address is already in use."});
+    //     }
+    //     if (userNameInUse) {
+    //       errors.push({msg: "Username is already in use."});
+    //     }
+    //     renderErrors(response, errors);
+    //   }
+    // }).catch( errors => {
+    //   bcrypt.hash(request.body.password, 10).then(hash => {
+    //     User.createUser(request.body.username, hash, '/', 0, request.body.email)
+    //       .then(users => {
+    //         request.flash('success_msg', "You are registered and can now login");
+    //         response.redirect('/');
+    //       });
+    //   });
 
-      })
-      .then((data) => {
-
-        User.isUserNameInUse(request.body.username)
-          .then((data) => {
-            // Username is also not unique
-            errors.push({
-              msg: "Username is already in use."
-            });
-
-            renderErrors(response, errors);
-
-          })
-          .catch((error) => {
-            renderErrors(response, errors);
-
-          });
-      })
-      .catch(error => {
-        // Email is unique, check if username is also unique
-        User.isUserNameInUse(request.body.username)
-          .then((data) => {
-            // Username is also not unique
-            errors.push({
-              msg: "Username is already in use."
-            });
-            renderErrors(response, errors);
-
-          })
-          .catch((error) => {
-            // Email and username are both unique
-            bcrypt.hash(request.body.password, 10).then( hash => {
-              User.createUser(request.body.username, hash, '/', 0, request.body.email)
-                .then(users => {
-                  request.flash('success_msg', "You are registered and can now login");
-                  response.redirect('/');
-                });
-            });
-          });
-
-      });
+   // });
   }
 });
 
-// Validate User
 let formValidation = ((request) => {
   request.checkBody('username', 'Username field cannot be empty.').notEmpty();
   request.checkBody('username', 'Username must be between 4-20 characters long.').len(4, 20);
