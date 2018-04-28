22887da3d094350a0cdc425e449b2bfd50a92ce8
jondo <jdombrowski416@gmail.com>
2018-04-28 15:42:48 -0700

    separating game and lobby chat, building to dynamic sockets

--- socket/index.js
+++ socket/index.js
@@ -6,28 +6,56 @@ const init = ( app, server ) => {
 
   app.set( 'io', io )
 
-  io.on( 'connection', function(socket){
-    console.log( 'client connected' )
+  // LOBBY CHAT 
+  var nsp_lobby = io.of('/lobby');
+  nsp_lobby.on('connection', function(socket){
+    console.log('lobby user connected');
 
-    socket.on( 'disconnect', function(socket){
-      console.log( 'client disconnected' )
+    nsp_lobby.on('disconnect' , function(socket){
+      console.log('lobby user disconnected')
     })
 
-    socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
+    nsp_lobby.on('chat message', function(msg){
+      console.log(' lobby message: ' + msg);
+      nsp.emit('chat message', 'everyone');
     });
+  });
+
+  // GAME ROOM CHAT
+  var nsp_game_room = io.of('/game');
+  nsp_game_room.on('connection', function(socket){
+    console.log('game user connected');
+
+    nsp_game_room.on('disconnect' , function(socket){
+      console.log('game user disconnected')
+    })
+
+    nsp_game_room.on('chat message', function(msg){
+      console.log(' lobby message: ' + msg);
+      nsp.emit('chat message', 'everyone');
+    });
+  });
+
+
+
+  // io.on( 'connection', function(socket){
+  //   console.log( 'client connected' )
+
+  //   socket.on( 'disconnect', function(socket){
+  //     console.log( 'client disconnected' )
+  //   })
+
+  //   socket.on('chat message', function(msg){
+  //     console.log('message: ' + msg);
+  //     io.emit('chat message', msg);
+  //   });
 
     
-    //idea for how to manage gamestate updates
-    socket.on('game_state_update', ({GAME_ID, GAME_STATE})=>{
-      socket.emit('game_state_update',{GAME_ID, GAME_STATE})
-    })
-  })
-}
-
-function sendMessage(data){
-  socket.emit('new_chat_message', data)
+  //   //idea for how to manage gamestate updates
+  //   socket.on('game_state_update', ({GAME_ID, GAME_STATE})=>{
+  //     socket.emit('game_state_update',{GAME_ID, GAME_STATE})
+  //   })
+  // })
 }
 
 module.exports = { init }
