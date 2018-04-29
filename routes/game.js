96cd120b0f496bb63b5ecf3ebcd88aeec583f3dd
khanhngg <khanhnguye94@gmail.com>
2018-04-28 19:00:20 -0700

    Routes for game

--- routes/game.js
+++ routes/game.js
@@ -1,22 +1,80 @@
 var express = require('express');
 var router = express.Router();
 
-// TODO: to be removed? temp for view
-// GET /game
-router.get('/', function(req, res, next) {
-  res.render('gameRoom', { title: 'UNO - Game Room' });
+/**
+ * CREATE GAME
+ */
+// GET /game -- Go to create game page
+router.get('/', function(request, response, next) {
+  response.render('createGame', {
+    title: 'UNO - Create Game'
+  });
 });
 
-// TODO: implement restful pattern
-// GET /game/:gameId
-// router.get('/:gameId', function(req, res, next) {
-//   res.render('gameRoom', { title: 'UNO - Game Room ID' });
-// });
+// POST /game -- Create a new game
+router.post('/', function(request, response, next) {
+  response.render('createGame', {
+    title: 'UNO - Create Game'
+  });
+  // TODO: redirect to GET /game/:gameId
+});
 
-// TODO: should make routes this way? 
-// GET /game/end
-router.get('/end', function( req, res, next ) {
-  res.render('endgamepage', { title: 'UNO - End Game' });
+/**
+ * GO TO SPECIFIC GAME ROOM
+ */
+// GET /game/:gameId -- Spectator or gameroom creator goes to a specific game room
+router.get('/:gameId', function(request, response, next) {
+  let gameId = request.params.gameId;
+  response.render('gameRoom', {
+    title: 'UNO - Game Room ' + gameId
+  });
+});
+
+// POST /game/:gameId -- A new player joins a specific game room
+router.post('/:gameId', function(request, response, next) {
+  let gameId = request.params.gameId;
+  response.render('gameRoom', {
+    title: 'UNO - Game Room ' + gameId
+  });
+});
+
+/**
+ * GAME LOGIC
+ */
+// POST /game/:gameId/draw -- Player requests a card from draw pile
+router.post('/:gameId/draw', function(request, response, next) {
+  let gameId = request.params.gameId;
+  response.render('gameRoom', {
+    title: 'UNO - Game Room ' + gameId
+  });
+});
+
+// POST /game/:gameId/play -- Player plays a card from their hand
+router.post('/:gameId/draw', function(request, response, next) {
+  let gameId = request.params.gameId;
+  response.render('gameRoom', {
+    title: 'UNO - Game Room ' + gameId
+  });
+});
+
+/**
+ * MESSAGING IN A SPECIFIC GAME ROOM
+ */
+// POST /game/:gameId/message -- Posting a message to game room
+router.post('/:gameId/message', function( request, response, next ) {
+  response.render('gameRoom', {
+    title: 'UNO - Message'
+  });
+});
+
+/**
+ * GAME ENDS
+ */
+// GET /game/:gameId/end -- Going to game end page
+router.get('/:gameId/end', function( request, response, next ) {
+  response.render('endGame', {
+    title: 'UNO - End Game'
+  });
 });
 
 module.exports = router;
