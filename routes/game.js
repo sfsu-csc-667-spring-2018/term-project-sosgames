c917f22139344dfbeef3aa4dc0f106d7eb95c375
Robert <rquinones93@yahoo.com>
2018-04-29 18:55:53 -0700

    All functions in games.js are now arrow functions

--- routes/game.js
+++ routes/game.js
@@ -5,14 +5,14 @@ var router = express.Router();
  * CREATE GAME
  */
 // GET /game -- Go to create game page
-router.get('/', function(request, response, next) {
+router.get('/', (request, response, next) => {
   response.render('createGame', {
     title: 'UNO - Create Game'
   });
 });
 
 // POST /game -- Create a new game
-router.post('/', function(request, response, next) {
+router.post('/', (request, response, next) => {
   response.render('createGame', {
     title: 'UNO - Create Game'
   });
@@ -23,7 +23,7 @@ router.post('/', function(request, response, next) {
  * GO TO SPECIFIC GAME ROOM
  */
 // GET /game/:gameId -- Spectator or gameroom creator goes to a specific game room
-router.get('/:gameId', function(request, response, next) {
+router.get('/:gameId', (request, response, next) => {
   let gameId = request.params.gameId;
   response.render('gameRoom', {
     title: 'UNO - Game Room ' + gameId
@@ -31,7 +31,7 @@ router.get('/:gameId', function(request, response, next) {
 });
 
 // POST /game/:gameId -- A new player joins a specific game room
-router.post('/:gameId', function(request, response, next) {
+router.post('/:gameId', (request, response, next) => {
   let gameId = request.params.gameId;
   response.render('gameRoom', {
     title: 'UNO - Game Room ' + gameId
@@ -42,7 +42,7 @@ router.post('/:gameId', function(request, response, next) {
  * GAME LOGIC
  */
 // POST /game/:gameId/draw -- Player requests a card from draw pile
-router.post('/:gameId/draw', function(request, response, next) {
+router.post('/:gameId/draw', (request, response, next) => {
   let gameId = request.params.gameId;
   response.render('gameRoom', {
     title: 'UNO - Game Room ' + gameId
@@ -50,7 +50,7 @@ router.post('/:gameId/draw', function(request, response, next) {
 });
 
 // POST /game/:gameId/play -- Player plays a card from their hand
-router.post('/:gameId/draw', function(request, response, next) {
+router.post('/:gameId/draw', (request, response, next) => {
   let gameId = request.params.gameId;
   response.render('gameRoom', {
     title: 'UNO - Game Room ' + gameId
@@ -61,7 +61,7 @@ router.post('/:gameId/draw', function(request, response, next) {
  * MESSAGING IN A SPECIFIC GAME ROOM
  */
 // POST /game/:gameId/message -- Posting a message to game room
-router.post('/:gameId/message', function( request, response, next ) {
+router.post('/:gameId/message', ( request, response, next ) => {
   response.render('gameRoom', {
     title: 'UNO - Message'
   });
@@ -71,7 +71,7 @@ router.post('/:gameId/message', function( request, response, next ) {
  * GAME ENDS
  */
 // GET /game/:gameId/end -- Going to game end page
-router.get('/:gameId/end', function( request, response, next ) {
+router.get('/:gameId/end', ( request, response, next ) => {
   response.render('endGame', {
     title: 'UNO - End Game'
   });
