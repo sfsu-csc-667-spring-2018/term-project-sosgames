162cb1136f9904238d179e5242fb380d312f23dc
khanhngg <khanhnguye94@gmail.com>
2018-05-06 17:14:43 -0700

    Fix session table migration

--- migrations/20180507000120-create-session.js
+++ migrations/20180507000120-create-session.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sessions', {
+    return queryInterface.createTable('session', {
       sid: {
         allowNull: false,
         primaryKey: true,
@@ -18,6 +18,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('sessions');
+    return queryInterface.dropTable('session');
   }
 };
