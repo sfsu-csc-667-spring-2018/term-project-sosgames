6c13c6c9783d56ff6ce7f02be9f09e73ba33bc52
khanhngg <khanhnguye94@gmail.com>
2018-05-13 00:34:14 -0700

    Fix create gamesCards crud to create game correctly in POST /game

--- gameEngine/dealCards.js
+++ gameEngine/dealCards.js
@@ -1,3 +1,5 @@
+const NUMBER_OF_CARDS = 7;
+
 const dealCards = cards => {};
 
 module.exports = dealCards;
--- routes/game.js
+++ routes/game.js
@@ -21,8 +21,8 @@ router.post('/', auth.requireAuthentication, (request, response, next) => {
 
   Games.create(gameName, numberOfPlayers)
     .then(gameData => {
-      UsersGames.create(user.id, gameData.id)
-        .then(userInGame => {
+      GamesCards.create(gameData.id)
+        .then(() => {
           response.redirect(`/game/${gameData.id}`);
         })
         .catch(error => {
@@ -47,6 +47,7 @@ router.get(
   (request, response, next) => {
     let gameId = request.params.gameId;
     let user = request.user;
+    console.log('got redirected to GET GAME');
 
     Games.findById(gameId)
       .then(game => {
@@ -121,19 +122,120 @@ router.get(
 router.post('/:gameId/start', (request, response, next) => {
   let gameId = request.params.gameId;
   let { clientSocketId, privateRoom } = request.body;
+
   let readyToStart = false;
+  let numberOfPlayers = 0,
+    maxNumberOfPlayers = 0;
+  let players = [];
 
-  // Find user.id and current number of players in users_games
-  // - UsersGames.findByGameId(gameId)
-  let numberOfPlayers = 2;
-  let players = [{ id: 1, username: 'dude' }, { id: 2, username: 'khanh' }];
+  Games.findById(gameId).then(gameData => {
+    maxNumberOfPlayers = gameData.max_number_of_players;
 
-  // Check if 2 <= number of players <= max_number_of_players in games table
-  // - Games.findById(gameId)
-  let maxNumberOfPlayers = 5;
-  if (numberOfPlayers >= 2 && numberOfPlayers <= maxNumberOfPlayers) {
-    readyToStart = true;
-  }
+    UsersGames.findByGameId(gameId).then(usersGamesData => {
+      // Check if valid number of players to start
+      numberOfPlayers = usersGamesData.length;
+      if (numberOfPlayers >= 2 && numberOfPlayers <= maxNumberOfPlayers) {
+        readyToStart = true;
+      }
+
+      // Get players' id in game
+      for (const userGame of usersGamesData) {
+        let player = {
+          userId: userGame.user_id,
+          numberOfCards: userGame.number_of_cards
+        };
+        players.push(player);
+      }
+
+      if (readyToStart) {
+        // - cardsInGame = GamesCards.findCardsByGameId(gameId) + Cards.getCards()
+
+        let cardsInGame = [
+          {
+            id: 1,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: '2',
+            color: 'red'
+          },
+          {
+            id: 2,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: '5',
+            color: 'blue'
+          },
+          {
+            id: 3,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: 'wild',
+            color: 'wild'
+          },
+          {
+            id: 4,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: 'reverse',
+            color: 'yellow'
+          },
+          {
+            id: 5,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: '1',
+            color: 'yellow'
+          },
+          {
+            id: 6,
+            inHand: false,
+            inDeck: false,
+            onTop: false,
+            value: '4',
+            color: 'green'
+          }
+        ];
+
+        // Pick 1 card on top
+        let cardOnTop = GameEngine.selectCardOnTop(cardsInGame);
+        // TODO: Update games_cards table
+
+        // Send game state to game room
+        // request.app.io.of(`/game/${gameId}`).emit('ready to start game', cardOnTop);
+
+        // Get players' private rooms in the same game
+        let rooms = request.app.io.sockets.adapter.rooms;
+        let playersRooms = [];
+        Object.keys(rooms).forEach(function(room) {
+          if (room.includes(`/game/${gameId}/`)) {
+            let userId = room.split('/')[3];
+            let playerInRoom = {};
+
+            playerInRoom.room = room;
+            playerInRoom.userId = userId;
+            playersRooms.push(playerInRoom);
+          }
+        });
+
+        for (const playerRoom of playersRooms) {
+          request.app.io.to(playerRoom.room).emit('yo', {
+            hello: `${playerRoom.userId} in room ${playerRoom.room}`
+          });
+        }
+
+        // TODO: deal card for each player
+        // grab user id from room
+        // use game engine to deal cards by user.id and card.id
+      } else {
+        request.app.io.of(`/game/${gameId}`).emit('not ready to start game');
+      }
+    });
+  });
 
   // If not valid --> app.io.emit('not ready')
 
@@ -143,93 +245,6 @@ router.post('/:gameId/start', (request, response, next) => {
   // -- GamesCards.dealToUser(gameId, user.id, card.id) --> set in_hand = true
   // Pick randomly 1 numbered card for on top from games_cards table
   // -- GamesCards.pickOnTop(gameId) --> set on_top = true
-
-  if (readyToStart) {
-    // - cardsInGame = GamesCards.findCardsByGameId(gameId) + Cards.getCards()
-    let cardsInGame = [
-      {
-        id: 1,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: '2',
-        color: 'red'
-      },
-      {
-        id: 2,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: '5',
-        color: 'blue'
-      },
-      {
-        id: 3,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: 'wild',
-        color: 'wild'
-      },
-      {
-        id: 4,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: 'reverse',
-        color: 'yellow'
-      },
-      {
-        id: 5,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: '1',
-        color: 'yellow'
-      },
-      {
-        id: 6,
-        inHand: false,
-        inDeck: false,
-        onTop: false,
-        value: '4',
-        color: 'green'
-      }
-    ];
-
-    // Pick 1 card on top
-    let cardOnTop = GameEngine.selectCardOnTop(cardsInGame);
-    // TODO: Update games_cards table
-
-    // Send game state to game room
-    // request.app.io.of(`/game/${gameId}`).emit('ready to start game', cardOnTop);
-
-    // Get players' private rooms in the same game
-    let rooms = request.app.io.sockets.adapter.rooms;
-    let playersRooms = [];
-    Object.keys(rooms).forEach(function(room) {
-      if (room.includes(`/game/${gameId}/`)) {
-        let userId = room.split('/')[3];
-        let playerInRoom = {};
-
-        playerInRoom.room = room;
-        playerInRoom.userId = userId;
-        playersRooms.push(playerInRoom);
-      }
-    });
-
-    for (const playerRoom of playersRooms) {
-      request.app.io.to(playerRoom.room).emit('yo', {
-        hello: `${playerRoom.userId} in room ${playerRoom.room}`
-      });
-    }
-
-    // TODO: deal card for each player
-    // grab user id from room
-    // use game engine to deal cards by user.id and card.id
-  } else {
-    request.app.io.of(`/game/${gameId}`).emit('not ready to start game');
-  }
 
   response.sendStatus(200);
 });
