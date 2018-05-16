4d00718b7132c57a50bb6b040a84cd466b23503e
niszeto <niszeto@gmail.com>
2018-05-15 17:25:21 -0700

    findTopCardByGameId works

---
+++ database/gamesCards/findTopCardByGameId.js
@@ -0,0 +1,11 @@
+const database = require('../connection');
+
+const FIND_TOP_CARD_BY_GAME_ID_QUERY = `SELECT * FROM games_cards INNER JOIN cards ON cards.id = games_cards.card_id WHERE games_cards.game_id = $1 AND on_top = true`;
+
+const findTopCardByGameId = gameId => {
+  return database.one(FIND_TOP_CARD_BY_GAME_ID_QUERY, [gameId]);
+};
+
+module.exports = {
+  findTopCardByGameId
+};
