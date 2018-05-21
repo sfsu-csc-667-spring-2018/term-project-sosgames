73b33aeedb4e246545be75230c5a6541c9bdefe3
Robert <rquinones93@yahoo.com>
2018-05-21 12:47:27 -0700

    - Game Table updates via sockets whenever a new game is created

--- database/games/createGame.js
+++ database/games/createGame.js
@@ -1,6 +1,6 @@
 const database = require('../connection');
-
-const CREATE_GAME_QUERY = `INSERT INTO games (name, max_number_of_players) VALUES ($1, $2) RETURNING id`;
+// CHANGE HERE TO RETURN ALL
+const CREATE_GAME_QUERY = `INSERT INTO games (name, max_number_of_players) VALUES ($1, $2) RETURNING *`;
 
 const create = (gameName, maxNumberOfPlayers) => {
   return database.one(CREATE_GAME_QUERY, [gameName, maxNumberOfPlayers]);
